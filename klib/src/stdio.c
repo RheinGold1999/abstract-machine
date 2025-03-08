8dfa1a8e8336ff08b032fffb37572be22e2b6242
rui zhang <keen_1999@163.com>
2025-03-08 15:56:44 +0800

    add stdio.c in klib

--- klib/src/stdio.c
+++ klib/src/stdio.c
@@ -5,8 +5,110 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// 反转字符串
+void reverse(char *str, int len) {
+  int i = 0, j = len - 1;
+  while (i < j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    i++;
+    j--;
+  }
+}
+
+// 将 int 转换为字符串
+int intToStr(int num, char *str) {
+  int i = 0;
+  int isNegative = 0;
+
+  // 处理负数
+  if (num < 0) {
+    isNegative = 1;
+    num = -num;
+  }
+
+  // 处理 0 的特殊情况
+  if (num == 0) {
+    str[i++] = '0';
+    str[i] = '\0';
+    return i;
+  }
+
+  // 逐位提取数字并转换为字符
+  while (num != 0) {
+    int digit = num % 10;
+    str[i++] = digit + '0';
+    num = num / 10;
+  }
+
+  // 添加负号（如果需要）
+  if (isNegative) {
+    str[i++] = '-';
+  }
+
+  // 反转字符串
+  reverse(str, i);
+  str[i] = '\0'; // 添加字符串结束符
+  return i;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  va_list ap;
+  int d;
+  char c;
+  char *s;
+
+  va_start(ap, fmt);
+  int i = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 's': {
+          s = va_arg(ap, char *);
+          while (*s) {
+            putch(*s);
+            i++;
+            s++;
+          }
+          break;
+        }
+
+        case 'd': {
+          d = va_arg(ap, int);
+          char num[32];
+          intToStr(d, num);
+          int n = 0;
+          while (num[n]) {
+            putch(num[n]);
+            i++;
+            n++;
+          }
+          break;
+        }
+
+        case 'c': {
+          c = (char) va_arg(ap, int);
+          putch(c);
+          i++;
+          break;
+        }
+
+        default: {
+          printf("%%%c is not supported.", *fmt);
+          assert(false);
+          break;
+        }
+      }
+    } else {
+      putch(*fmt);
+      i++;
+      fmt++;
+    }
+  }
+  return i;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -14,7 +116,62 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  va_list ap;
+  int d;
+  char c;
+  char *s;
+
+  va_start(ap, fmt);
+  int i = 0;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 's': {
+          s = va_arg(ap, char *);
+          while (*s) {
+            out[i] = *s;
+            i++;
+            s++;
+          }
+          break;
+        }
+
+        case 'd': {
+          d = va_arg(ap, int);
+          char num[32];
+          intToStr(d, num);
+          int n = 0;
+          while (num[n]) {
+            out[i] = num[n];
+            i++;
+            n++;
+          }
+          break;
+        }
+
+        case 'c': {
+          c = (char) va_arg(ap, int);
+          out[i] = c;
+          i++;
+          break;
+        }
+
+        default: {
+          printf("%%%c is not supported.", *fmt);
+          assert(false);
+          break;
+        }
+      }
+    } else {
+      out[i] = *fmt;
+      i++;
+      fmt++;
+    }
+  }
+  out[i+1] = '\0';
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
